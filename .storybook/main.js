37ff80bd0bfb7039d895b362c7c5d22c29b485e0
jonyworker <jonywu@gmail.com>
2024-10-05 18:37:44 +0800

    ✨feat：新增 storybook add-on [Vue Slot]。
    - 在 stories 中控制 Vue slot
    - https://storybook.js.org/addons/storybook-addon-vue-slots

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,8 @@ const config = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-viewport",
+    "@whitespace/storybook-addon-html", // 👈 story 中程式碼檢視功能
+    'storybook-addon-vue-slots', // 👈 處理 vue slot
   ],
   framework: {
     name: "@storybook/vue3-vite",
