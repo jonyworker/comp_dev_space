a19272edb7f5e200cf27422209492992c6754cd3
jonyworker <jonywu@gmail.com>
2024-10-06 03:37:40 +0800

    🆙update：Toast.stories.js, Toast.vue 和 useToast.js。
    [Toast.stories.js]
    - 調整屬性順序
    - 隱藏不必要 control
    - 增加基礎樣式 storybook 用關閉功能
    - storybook control 功能測試 ok
    
    [Toast.vue]
    - 調整屬性順序
    - 重構部分程式碼
     - 調整關閉邏輯，由元純外部控制改為內部duration emit 外部控制
     - 刪除 themeColor, show, id 屬性
    - 取消 content slot 改由 props 傳值
    
    [useToast.js]
    - 取消由 useToast 控制 Toast 顯示時間

---
+++ src/page/PageToast.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Toast from "@/ui/element/Toast/Toast.vue";
+import { useToast } from "@/composables/useToast.js";
+import Button from "@/ui/element/Button/Button.vue";
+
+// Toast 所需資料
+const { add, toasts, remove } = useToast();
+const showSuccess = () => {
+	add({
+		title: "Success Message",
+		message: "Hello, world! This is a toast message.",
+		severity: "success",
+		showToast: true,
+	});
+};
+const showError = () => {
+	add({
+		title: "Error Message",
+		message: "Hello, world! This is a toast message.",
+		severity: "error",
+		showToast: true,
+	});
+};
+
+
+</script>
+
+<template>
+	<!-- Toast -->
+	<div style="display: flex; gap: 16px">
+		<Button themeColor="success" variant="outlined" @click="showSuccess"
+		>Show Success Toast</Button
+		>
+		<Button themeColor="error" variant="outlined" @click="showError"
+		>Show Success Toast</Button
+		>
+	</div>
+
+	<Toast
+		v-for="toast in toasts"
+		:key="toast.id"
+		:title="toast.title"
+		:message="toast.message"
+		:severity="toast.severity"
+		:duration= 1000
+		@close="remove(toast.id)"
+	></Toast>
+</template>
+
+<style scoped lang="scss">
+
+</style>
