24279373e5704a62983cf562aa34f4a7908a9137
jonyworker <jonywu@gmail.com>
2024-10-27 23:16:47 +0800

    ✨feat：新增 Menu 元件

---
+++ src/page/PageMenu.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Menu from "@/ui/element/Menu/Menu.vue"
+const menuData = [
+	{
+		icon: "home",
+		label: "首頁",
+		path: "/",
+	},
+	{
+		icon: "users",
+		label: "用戶管理",
+		path: "/users",
+		children: [
+			{ label: "用戶列表", path: "/users/list" },
+			{ label: "用戶設置", path: "/users/settings" },
+		],
+	},
+	{
+		icon: "file",
+		label: "文件管理",
+		path: "/files",
+		children: [
+			{ label: "所有文件", path: "/files/all" },
+			{ label: "已分享", path: "/files/shared" },
+		],
+	},
+	{
+		icon: "mail",
+		label: "訊息中心",
+		path: "/messages",
+	},
+	{
+		icon: "setting",
+		label: "系統設置",
+		path: "/settings",
+	},
+];
+</script>
+
+<template>
+	<Menu :menuData="menuData" :isExpanded="true"></Menu>
+</template>
+
+<style scoped lang="scss">
+
+</style>
