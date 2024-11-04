acb17a2fdd5101ddcf6d8cee96b801d86b45dc1d
jonyworker <jonywu@gmail.com>
2024-11-05 00:48:22 +0800

    🆙update：SideNav - 刪除多餘 computed definedEmits

--- src/ui/element/SideNavMenu/SideNavMenu.vue
+++ src/ui/element/SideNavMenu/SideNavMenu.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, defineEmits } from "vue";
+import { ref } from "vue";
 import Icon from "@/ui/element/Icon/Icon.vue";
 
 const isExpandedModelValue = defineModel('isExpanded')
