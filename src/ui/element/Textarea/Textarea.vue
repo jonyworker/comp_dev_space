f8f3a9f3815b98051b13669fab4b19be2042702a
jonyworker <jonywu@gmail.com>
2024-11-14 02:13:05 +0800

    🆙update：Textarea - 單引號改雙引號 - 修改 Prop 順序 - 增加 initValue

--- src/ui/element/Textarea/Textarea.vue
+++ src/ui/element/Textarea/Textarea.vue
@@ -1,46 +1,66 @@
 <script setup>
-	import { computed } from 'vue';
+import { computed, watch } from 'vue';
 
-	// 定義 Model
-	const modelValue = defineModel()
+// 定義 Model
+const modelValue = defineModel();
 
-	// 定義 Props
-	const props = defineProps({
-		label: {
-			type: String,
-		},
-		placeholder: {
-			type: String,
-			default: "placeholder text",
-		},
-		limit: {
-			type: Number,
-			default: 100,
-		},
-		isDisabled: {
-			type: Boolean,
-			default: false,
-		},
-		hint: {
-			type: Object,
-			default: () => ({ error: '', description: '' }),
-		},
-		className: {
-			type: String,
-			default: '',
-		},
-	})
+// 定義 Props
+const props = defineProps({
+	label: {
+		type: String,
+	},
+	placeholder: {
+		type: String,
+		default: "請輸入...",
+	},
+	limit: {
+		type: Number,
+		default: 100,
+	},
+	initValue: {
+		type: String,
+		default: "",
+	},
+	hint: {
+		type: Object,
+		default: () => ({ error: "", description: "" }),
+	},
+	isDisabled: {
+		type: Boolean,
+		default: false,
+	},
+	className: {
+		type: String,
+		default: "",
+	},
+});
 
-	// 根據 hint 的值，計算屬性
-	const hintClass = computed(() => {
-		if (props.hint.error.length > 0) {
+
+// 初始化 modelValue
+if (!modelValue.value) {
+	modelValue.value = props.initValue;
+}
+
+// 根據 hint 的值，計算屬性
+const hintClass = computed(() => {
+	if (props.hint.error.length > 0) {
 		return 'error';
 	}
 	if (props.hint.description.length > 0) {
 		return 'desc';
 	}
-		return '';
-	});
+	return '';
+});
+
+// 同步 props.initValue 的變化到 modelValue
+watch(
+	() => props.initValue,
+	(newValue) => {
+		if (!modelValue.value) {
+			modelValue.value = newValue;
+		}
+	}
+);
 </script>
 
 <template>
@@ -57,30 +77,30 @@
 			( props.hint.error.length > 0 && `textarea-border-${hintClass}`)]">
 			<!-- 多行輸入框 -->
 			<textarea id="id"
-			:class="['textarea', {'textarea-disable': props.isDisabled}]"
-			:maxlength="props.limit > 0 ? props.limit : undefined"
-			:placeholder="props.placeholder"
-			v-model="modelValue"
-			@input="handleInput"></textarea>
+			          :class="['textarea', {'textarea-disable': props.isDisabled}]"
+			          :maxlength="props.limit > 0 ? props.limit : undefined"
+			          :placeholder="props.placeholder"
+			          v-model="modelValue"
+			></textarea>
 
 			<!-- 多行輸入字數提示 -->
 			<template v-if="modelValue.length > 0 && props.limit === 0">
 				<small :class="['textarea-hint-count', {'textarea-disable': props.isDisabled}]">
-					{{ (modelValue.length > 0 ? `${modelValue.length}` : '') }}
+					{{ modelValue.length }}
 				</small>
 			</template>
 
 			<template v-else>
 				<small :class="['textarea-hint-count', {'textarea-disable': props.isDisabled}]">
-					{{ (modelValue.length > 0 ? `${modelValue.length} / ${props.limit}` : '') }}
+					{{ modelValue.length > 0 ? `${modelValue.length} / ${props.limit}` : '' }}
 				</small>
 			</template>
 		</div>
 
 		<!-- 多行輸入說明文字與提示 -->
 		<template v-if="props.hint.error.length > 0 || props.hint.description.length > 0">
-			<small :class="['textarea-hint',{'textarea-disable': props.isDisabled},`textarea-hint-${hintClass}`]">
-				{{props.hint.error.length > 0 ? props.hint.error : props.hint.description}}
+			<small :class="['textarea-hint', {'textarea-disable': props.isDisabled}, `textarea-hint-${hintClass}`]">
+				{{ props.hint.error.length > 0 ? props.hint.error : props.hint.description }}
 			</small>
 		</template>
 	</div>
