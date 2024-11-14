90a56aec6211d278a6dd570b5343e8d0e56cca2d
jony wu <37248488+jonyworker@users.noreply.github.com>
2024-11-14 16:17:15 +0800

    ✨feat：新增心寫法的Menu(自用)。

--- src/App1.vue
+++ src/App1.vue
@@ -1,10 +1,53 @@
 <script setup>
+import MenuMain from '@/ui/element/Menu_Recursive_Jony/Menu_Main.vue'
 
-
+const dataSource = [
+    {
+        icon: "home",
+        label: "首頁",
+        path: "/",
+    },
+    {
+        icon: "users",
+        label: "用戶管理",
+        path: "/users",
+        children: [
+            { label: "用戶列表", path: "/users/list" },
+            { label: "用戶設置", path: "/users/settings",children: [
+                    { label: "用戶列表", path: "/users/list" },
+                    { label: "用戶設置", path: "/users/settings" },
+                ], },
+        ],
+    },
+    {
+        icon: "file",
+        label: "文件管理",
+        path: "/files",
+        children: [
+            { label: "所有文件", path: "/files/all" },
+            { label: "已分享", path: "/files/shared" },
+        ],
+    },
+    {
+        icon: "mail",
+        label: "訊息中心",
+        path: "/messages",
+    },
+    {
+        icon: "setting",
+        label: "系統設置",
+        path: "/settings",
+    },
+];
 </script>
 
 <template>
+<MenuMain
+    :dataSource="dataSource">
 
+</MenuMain>
+
+<!--    <Menu :dataSource="dataSource"></Menu>-->
 
 </template>
 
