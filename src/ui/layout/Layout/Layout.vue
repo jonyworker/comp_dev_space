655182d76afe44c9a5ccb23953af92d8cf97d9b7
jonyworker <jonywu@gmail.com>
2024-11-19 22:51:07 +0800

    ✨feat：Layout(ing)

---
+++ src/ui/layout/Layout/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+	// 定義 Props
+	const defineProps = defineProps({
+		className: {
+			type: String,
+			default: "",
+		}
+	})
+</script>
+
+<template>
+	<div :class="{'ded-footer': true, [props.className]: !!props.className}"></div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
