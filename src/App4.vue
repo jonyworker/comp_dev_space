cff4399dbea3aa74bce62383e9992a66a04141b3
jonyworker <jonywu@gmail.com>
2024-12-22 14:05:46 +0800

    ✨feat：Pagination - 新增 Vue - 新增 storybook - 新增 icons

---
+++ src/App4.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Pagination from "@/ui/element/Pagination/Pagination.vue"
+
+
+
+</script>
+
+<template>
+	<Pagination
+		:totalItems="100"
+		:currentPage="1"
+		:itemsPerPageOptions="[10, 20, 50]"
+		:defaultItemsPerPage="5"
+		:isShowPageInfo="true"
+		className=""
+	>
+	</Pagination>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import '@/style/globals.scss'
-import App from './App3.vue'
+import App from './App4.vue'
 
 createApp(App).mount('#app')
