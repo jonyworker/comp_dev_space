a84311a6719c53557521f77b804adc6438f0a187
jony wu <37248488+jonyworker@users.noreply.github.com>
2024-12-27 21:18:05 +0800

    🆙update：Navbar(ing)

---
+++ src/App5.vue
@@ -0,0 +1,41 @@
+<!-- DemoPage.vue (範例父層) -->
+<script setup>
+import { ref } from 'vue'
+import Navbar from '@/ui/element/Navbar/Navbar.vue'
+
+const links = ref([
+    {
+        "label": "Products",
+        "href": "#products",
+        "order": 1
+    },
+    {
+        "label": "Solutions",
+        "href": "#solutions",
+        "order": 2
+    },
+    {
+        "label": "About",
+        "href": "#about",
+        "order": 3
+    },
+    {
+        "label": "Technologies",
+        "href": "#technologies",
+        "order": 4
+    }
+])
+
+</script>
+
+<template>
+    <Navbar
+        :links="links"
+    ></Navbar>
+</template>
+
+
+
+<style scoped>
+
+</style>
