f804f3a707d63452ab115b1692999c6bec0f5bbe
jonyworker <jonywu@gmail.com>
2025-02-23 23:08:11 +0800

    ✨feat：build前準備 - 修改 vue   1. 修改動態生成

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import path from 'path'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),svgLoader()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+	plugins: [
+		vue(),
+		svgLoader({
+			defaultExport: "component" // 確保 SVG 被當作 Vue 元件使用
+		})
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.js"), // 入口文件
+			name: "CompDevSpace", // UMD 格式需要一個全局名稱
+			formats: ["es", "cjs", "umd"], // 記得加上 "cjs"，讓它可以正確輸出
+			fileName: (format) => `comp-dev-space.${format}.js`,
+		},
+		rollupOptions: {
+			// external 確保 Vue 不會被打包進去
+			external: ["vue"],
+			output: {
+				globals: {
+					vue: "Vue",
+				},
+				name: "CompDevSpace", // 這是 UMD 格式必要的
+			},
+		},
+	},
 })
