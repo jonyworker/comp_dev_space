c9a7f270586e4d5f5199d09745deae9d4c0cb0f0
jony wu <37248488+jonyworker@users.noreply.github.com>
2025-03-10 19:54:23 +0800

    🆙update：Button - 改成 Type Script 寫法

--- src/ui/element/Button/Button.vue
+++ src/ui/element/Button/Button.vue
@@ -1,66 +1,34 @@
-<script setup>
+<script setup lang="ts">
 import Icon from '@/ui/element/Icon/Icon.vue';
 
-// 定義 Props
-const props = defineProps({
-	themeColor: {
-		type: String,
-		default: "primary",
-		validator: (value) =>
-			[
-				"primary",
-				"secondary",
-				"neutral",
-				"info",
-				"success",
-				"warning",
-				"error",
-			].includes(value),
-	},
-	variant: {
-		type: String,
-		required: true,
-		validator: (value) => ["text", "filled", "ghost", "soft"].includes(value),
-	},
-	prefix: {
-		type: String,
-	},
-	suffix: {
-		type: String,
-	},
-	size: {
-		type: String,
-		default: "medium",
-		validator: (value) => ["small", "medium", "large"].includes(value),
-	},
-	width: {
-		type: String,
-		default: "fit",
-		validator: (value) => ["fluid", "fit"].includes(value),
-	},
-	borderWidth: {
-		type: String,
-		validator: (value) => ["none", "1px", "2px", "3px", "4px", "5px"].includes(value),
-	},
-	radius: {
-		type: String,
-		validator: (value) => ["none", "2px", "4px", "8px", "12px", "16px", "32px", "full"].includes(value),
-	},
-	isDisabled: {
-		type: Boolean,
-		default: false,
-	},
-	className: {
-		type: String,
-		default: "",
-	},
+// 定義一個 Props 介面
+interface Props {
+    themeColor?: "primary" | "secondary" | "neutral" | "info" | "success" | "warning" | "danger"
+    variant: "text" | "filled" | "ghost" | "soft"
+    prefix?: string
+    suffix?: string
+    size?: "small" | "medium" | "large"
+    width?: "fluid" | "fit"
+    borderWidth?: "none" | "1px" | "2px" | "3px" | "4px" | "5px"
+    radius?: "none" | "2px" | "4px" | "8px" | "12px" | "16px" | "32px" | "full"
+    isDisabled?: boolean
+    className?: string
+}
+
+// 定義 Props default 值
+const props = withDefaults(defineProps<Props>(),{
+    themeColor: "primary",
+    size: "medium",
+    width: "fit",
+    isDisabled: false,
+    className: "",
 });
 
 </script>
 
 <template>
-<!--	<button :class="finalButtonClass">-->
-	<button :class="{
+    <!--	<button :class="finalButtonClass">-->
+    <button :class="{
         'ded-button': true,
         [`ded-button-${props.width}`]: props.width,
         [`ded-text-${props.size}`]: props.size,
@@ -72,24 +40,24 @@ const props = defineProps({
         [props.className]: !!props.className,
         // 'ded-button-only-icon' : !slot
     }">
-		<template v-if="prefix">
-			<div :class="`ded-icon-${props.size}`">
-				<Icon :name="props.prefix"></Icon>
-			</div>
-		</template>
+        <template v-if="prefix">
+            <div :class="`ded-icon-${props.size}`">
+                <Icon :name="props.prefix"></Icon>
+            </div>
+        </template>
 
-		<template v-if="$slots.default" >
-			<div class="ded-button-content">
-				<slot></slot>
-			</div>
-		</template>
+        <template v-if="$slots.default" >
+            <div class="ded-button-content">
+                <slot></slot>
+            </div>
+        </template>
 
-		<template v-if="suffix">
-			<div :class="`ded-icon-${props.size}`">
-				<Icon :name="props.suffix"></Icon>
-			</div>
-		</template>
-	</button>
+        <template v-if="suffix">
+            <div :class="`ded-icon-${props.size}`">
+                <Icon :name="props.suffix"></Icon>
+            </div>
+        </template>
+    </button>
 </template>
 
 <style lang="scss" scoped>
